d3eaf49615c7ade109c7a06d21fee2bad1792b14
Jessie Chatham Spencer <jessie@teainspace.com>
2024-06-01 15:40:28 +0200

    Add diagnostics buffer to telescope.

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -75,6 +75,7 @@ end, { desc = '[t]elescope find files - ctrl[p] style' })
 vim.keymap.set('n', '<M-p>', builtin.oldfiles, { desc = '[telescope] old files' })
 vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = '[telescope] live grep' })
 vim.keymap.set('n', '<leader>tf', fuzzy_grep, { desc = '[t]elescope [f]uzzy grep' })
+vim.keymap.set('n', '<leader>te', builtin.diagnostics, { desc = '[t]elescope [f]uzzy grep' })
 vim.keymap.set('n', '<M-f>', fuzzy_grep_current_file_type, { desc = '[telescope] fuzzy grep filetype' })
 vim.keymap.set('n', '<M-g>', live_grep_current_file_type, { desc = '[telescope] live grep filetype' })
 vim.keymap.set(
